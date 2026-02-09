16d5b5459e125dc672e709f74e91547b3980f0a2
Salina Bee <siriuspilas@gmail.com>
2026-02-08 22:44:37 -0500

    adjusted CORS permissions

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
-    allow_methods=["POST"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
