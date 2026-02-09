e5e3ceaa8a55bb76359b0f50da9e3d181eb02def
Salina Bee <siriuspilas@gmail.com>
2026-02-08 19:35:26 -0500

    Initial background remover project

---
+++ app.js
@@ -0,0 +1,32 @@
+const imageInput = document.getElementById("imageInput");
+const removeBtn = document.getElementById("removeBtn");
+const resultImage = document.getElementById("resultImage");
+const downloadLink = document.getElementById("downloadLink");
+
+removeBtn.addEventListener("click", async () => {
+    
+    // read image provided by user
+    const file = imageInput.files[0];
+
+    if (!file) {
+        return;
+    }
+
+    // wrap in FormData so image can be sent to backend
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // send to /remove-bg endpoint
+    const response = await fetch("http://localhost:8000/remove-bg", {
+        method: "POST",
+        body: formData,
+    });
+
+    // receive processed image
+    const blob = await response.blob(); // reads response as raw binary data; used when API returns images, PDFs, audio, or other non-text files
+    const imageUrl = URL.createObjectURL(blob);
+
+    resultImage.src = imageUrl; // display image
+    downloadLink.href = imageUrl; // provide image download link
+    downloadLink.style.display = "inline";
+});
